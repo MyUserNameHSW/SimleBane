5f9a771be712eb09ee03695666969d50f2b2066c
xiaohe@xiaoshijie.com <xiaoshijie>
2019-04-26 16:15:53 +0800

    init

---
+++ test001/simples/src/main/java/cn/izy1314/simples/NetSchool.java
@@ -0,0 +1,12 @@
+package cn.izy1314.simples;
+
+/**
+ * @author heshuai
+ * created on: 2019/4/26 4:14 PM
+ * description:
+ */
+public class NetSchool {
+    public static void setNum(int num){
+
+    }
+}
